e7e2723336fd0ed17c62992f477c86357bb353d6
Anupam <anupamsingh2389@gmail.com>
2023-10-17 19:10:45 +0530

    Create check_even_odd.js

---
+++ JavaScript/check_even_odd.js
@@ -0,0 +1,9 @@
+// Returns true if n is even, else odd 
+function isEven(n) { 
+    return (n % 2 == 0); 
+} 
+  
+// Driver code 
+let n = 101; 
+  
+isEven(n) ? console.log("Even") : console.log("Odd");
